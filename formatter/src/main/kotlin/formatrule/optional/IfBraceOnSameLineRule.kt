a907b239630d1249f5d57b1a7838a2b11d9f8c8a
SergioMorero <smorero@mail.austral.edu.ar>
2025-11-11 20:53:09 -0300

    Modified 'If Brace' formatting rules to detect errors correctly

--- formatter/src/main/kotlin/formatrule/optional/IfBraceOnSameLineRule.kt
+++ formatter/src/main/kotlin/formatrule/optional/IfBraceOnSameLineRule.kt
@@ -4,88 +4,60 @@ import container.src.main.kotlin.Container
 import formatter.src.main.kotlin.formatrule.FormatRule
 import token.src.main.kotlin.Token
 import tokendata.src.main.kotlin.DataType
-import tokendata.src.main.kotlin.Position
 
 class IfBraceOnSameLineRule : FormatRule {
-
-    private val identifier = DataType.IDENTIFIER
-    private val openBrace = DataType.OPEN_BRACE
-    private val closeParen = DataType.CLOSE_PARENTHESIS
-    private val lineBreak = DataType.LINE_BREAK
-    private val space = DataType.SPACE
-
     override fun format(source: Container): Container {
-        var tokens = source
+        val newTokens = mutableListOf<Token>()
         var i = 0
+        while (i < source.size()) {
+            val currentToken = source.get(i)!!
 
-        while (i < tokens.size()) {
-            val token = tokens.get(i) ?: break
+            if (currentToken.type == DataType.IF_KEYWORD) {
+                var closingParenIndex = -1
+                var openBraceIndex = -1
 
-            if (token.type == identifier && token.content == "if") {
-                val closeParenIndex = findCloseParenthesis(tokens, i)
-
-                if (closeParenIndex != -1) {
-                    val braceIndex = findOpenBrace(tokens, closeParenIndex)
-
-                    if (braceIndex != -1) {
-                        // Remover todo entre ) y {
-                        var currentBraceIndex = braceIndex
-                        while (closeParenIndex + 1 < currentBraceIndex) {
-                            val tokenBetween = tokens.get(closeParenIndex + 1)
-                            if (tokenBetween?.type == lineBreak || tokenBetween?.type == space) {
-                                val response = tokens.remove(closeParenIndex + 1)
-                                tokens = response.container
-                                if (response.token == null) break
-                                currentBraceIndex--
-                            } else {
-                                break
-                            }
-                        }
-
-                        // Agregar un espacio
-                        tokens = tokens.addAt(
-                            Token(space, " ", Position(0, 0)),
-                            closeParenIndex + 1
-                        )
+                // Encontrar el ')' del if
+                for (j in i + 1 until source.size()) {
+                    if (source.get(j)!!.type == DataType.CLOSE_PARENTHESIS) {
+                        closingParenIndex = j
+                        break
                     }
                 }
+
+                if (closingParenIndex != -1) {
+                    // Buscar el '{' que le sigue, saltando espacios y saltos de línea
+                    var nextTokenIndex = closingParenIndex + 1
+                    while (nextTokenIndex < source.size() &&
+                        (source.get(nextTokenIndex)!!.type == DataType.SPACE || source.get(nextTokenIndex)!!.type == DataType.LINE_BREAK)
+                    ) {
+                        nextTokenIndex++
+                    }
+
+                    if (nextTokenIndex < source.size() && source.get(nextTokenIndex)!!.type == DataType.OPEN_BRACE) {
+                        openBraceIndex = nextTokenIndex
+                    }
+                }
+
+                if (openBraceIndex != -1) {
+                    // Añadir todos los tokens desde 'if' hasta ')'
+                    for (j in i..closingParenIndex) {
+                        newTokens.add(source.get(j)!!)
+                    }
+                    // Añadir un espacio
+                    newTokens.add(Token(DataType.SPACE, " ", currentToken.position))
+                    // Añadir la llave
+                    newTokens.add(source.get(openBraceIndex)!!)
+
+                    // Avanzar el índice principal
+                    i = openBraceIndex
+                } else {
+                    newTokens.add(currentToken)
+                }
+            } else {
+                newTokens.add(currentToken)
             }
             i++
         }
-
-        return tokens
-    }
-
-    private fun findCloseParenthesis(tokens: Container, startIndex: Int): Int {
-        var parenCount = 0
-        var j = startIndex + 1
-
-        while (j < tokens.size()) {
-            val currentToken = tokens.get(j) ?: break
-            when (currentToken.type) {
-                DataType.OPEN_PARENTHESIS -> parenCount++
-                closeParen -> {
-                    parenCount--
-                    if (parenCount == 0) return j
-                }
-
-                else -> {}
-            }
-            j++
-        }
-        return -1
-    }
-
-    private fun findOpenBrace(tokens: Container, startIndex: Int): Int {
-        var j = startIndex + 1
-        while (j < tokens.size()) {
-            val currentToken = tokens.get(j) ?: break
-            when (currentToken.type) {
-                openBrace -> return j
-                space, lineBreak -> j++
-                else -> return -1 // Encontramos otro token, no hay llave
-            }
-        }
-        return -1
+        return Container(newTokens)
     }
 }
