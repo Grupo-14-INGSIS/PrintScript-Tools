acd121660090ae914dd9333ff845e85b36e90714
SergioMorero <smorero@mail.austral.edu.ar>
2025-12-01 02:00:12 -0300

    .

--- interpreter/src/main/kotlin/Util.kt
+++ interpreter/src/main/kotlin/Util.kt
@@ -3,6 +3,11 @@ package interpreter.src.main.kotlin
 fun Any?.formatNumber(): String {
     return when (this) {
         is Double -> if (this % 1.0 == 0.0) this.toInt().toString() else this.toString()
+        is String -> if (this.startsWith("\"") && this.endsWith("\"")) {
+            this.substring(1, this.length - 1)
+        } else {
+            this.toString()
+        }
         else -> this.toString()
     }
 }
